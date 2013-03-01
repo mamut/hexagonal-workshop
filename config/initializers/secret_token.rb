a5dd5c6cba2370b3d942ff59df63998e0688699d
Krzysztof Urban <krzysztof.urban@netguru.pl>
2013-03-01 15:10:31 +0100

    hexagonaljs workshop initial state

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hexagonal::Application.config.secret_token = 'b08ca00909252e4afe458c0727a62c4bd15efabb635cb72ffeb1d65d19401c0bf83edca5f712aa4e7a279935505e7086e27e9b5ea95e281a0b1b6d37e5199a65'
